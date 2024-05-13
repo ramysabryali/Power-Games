10a0b34e935a4461b9950dd3dcddf13fd61ea75b
Ramy Sabry <ramysabry1996@gmail.com>
2024-05-13 21:02:01 +0300

    Initial Commit

---
+++ PowerGames/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PowerGames
+//
+//  Created by Ramy Sabry on 13/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PowerGames/PowerGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerGamesApp.swift
+//  PowerGames
+//
+//  Created by Ramy Sabry on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PowerGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
